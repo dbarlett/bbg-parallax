be3e6f12ff378637b6e2a495b59ca119a9d9db84
Dylan Barlett <dylan.barlett@gmail.com>
2015-03-31 20:26:37 -0400

    Spacing per WP Code Standards

--- functions.php
+++ functions.php
@@ -1,35 +1,37 @@
-<?php // load parent theme CSS
-
-error_reporting(E_ALL);ini_set('display_errors', 1);
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
 //define('WP_DEBUG', true);
 
+// load parent theme CSS
+add_action( 'wp_enqueue_scripts', 'load_parent_theme_css' );
 function load_parent_theme_css() {
-    wp_enqueue_style( 'parent-theme', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent-theme', get_template_directory_uri() . '/style.css' );
 }
 
-add_action( 'wp_enqueue_scripts', 'load_parent_theme_css' );
+add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts', 12 );
+function custom_enqueue_scripts() {
+	wp_deregister_style( 'themify-media-queries' );
+	wp_enqueue_style( 'themify-media-queries', get_stylesheet_directory_uri() . '/media-queries.css' );
+}
 
-
-function custom_enqueue_scripts() { wp_deregister_style( 'themify-media-queries' ); wp_enqueue_style( 'themify-media-queries', get_stylesheet_directory_uri() . '/media-queries.css' ); } add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts', 12 );
-
-
-add_action("gform_after_submission_3", "acf_post_submission", 10, 2);
-function acf_post_submission ($entry, $form) {
+add_action( 'gform_after_submission_3', 'acf_post_submission', 10, 2 );
+function acf_post_submission($entry, $form ) {
 	$field_key = 'field_54817fefd7196';
 	$user_id = 'user_' . get_current_user_id();
 	$value = array();
-	$value += get_field($field_key, $user_id);
+	$value += get_field( $field_key, $user_id );
 	// fix the date format, no hyphen!
-	foreach($value as $key => &$val) {
-		$val['date'] = str_replace('-', '', $val['date']);
+	foreach ( $value as $key => &$val ) {
+		$val['date'] = str_replace( '-', '', $val['date'] );
 	}
 	$value[] = array(
-		'date' => date("Ymd", strtotime($entry[2])),
+		'date'                          => date( 'Ymd', strtotime( $entry[2] ) ),
 		'total_nutrition_points_earned' => $entry[6],
-		'total_fitness_points_earned' => $entry[7],
-		'total_wellness_points_earned' => $entry[8]
+		'total_fitness_points_earned'   => $entry[7],
+		'total_wellness_points_earned'  => $entry[8],
 	);
 	// print_r($value); die;
-	error_log(print_r($value,1), 3, "/tmp/my-errors.log");
-	update_field($field_key, $value, $user_id);
+	error_log( print_r( $value, 1 ), 3, '/tmp/my-errors.log' );
+	update_field( $field_key, $value, $user_id );
 }
